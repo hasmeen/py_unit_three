6b297cc4fac2f838eefaefc0af3e4001fd7115c3
hasmeen <112644288+hasmeen@users.noreply.github.com>
2022-10-06 11:26:47 -0400

    commmiting 3

--- egg.py
+++ egg.py
@@ -1,9 +1,23 @@
 # Start writing your functions below this line
 
+def top()
+    print(,__)
+    print()
+    print()
 
+def mid()
+    print()
+    print()
+    print()
+
+def bottom()
+    print()
+    print()
+    print()
 def main():
-    # all of your function calls should go here. Remove the word "pass" before adding function calls.
-    pass
+    top()
+    mid()
+    bottom()
 
 if __name__ == '__main__':
     main()
